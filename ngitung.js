03ec32ee283031556732eb4358344299676934a7
yanwarS <103078039+yanwarS@users.noreply.github.com>
2022-04-25 09:22:18 +0700

    Update ngitung.js

--- ngitung.js
+++ ngitung.js
@@ -1,19 +1,18 @@
 function throughput(){
     var jmlDataBps=parseFloat(document.getElementById("jmlDataBps").value);
     var waktuPengiriman=parseFloat(document.getElementById("waktuPengiriman").value);
-    var throughput1=jmlDataBps/waktuPengiriman;
-    var hasil=throughput1/200;
+    var hasil = jmlDataBps/waktuPengiriman;
     document.getElementById("hasilThroughput").value=hasil;
-    if (hasil <= 25){
+    if (parseInt(hasil) <= 25){
         document.getElementById("indeksThroughput").value=4;
     }
-    else if (hasil <= 50){
+    else if (parseInt(hasil) <= 50){
         document.getElementById("indeksThroughput").value=3;
     }
-    else if (hasil <= 75){
+    else if (parseInt(hasil) <= 75){
         document.getElementById("indeksThroughput").value=2;
     }
-    else if (hasil <= 100){
+    else if (parseInt(hasil) <= 100){
         document.getElementById("indeksThroughput").value=1;
     }
 }
@@ -22,36 +21,36 @@ function delay(){
     var paketDiterima=parseFloat(document.getElementById("paketDiterima").value);
     var hasil=totalDelay/paketDiterima;
     document.getElementById("hasilDelay").value=hasil;
-    if (hasil < 150){
-        document.getElementById("indeksPacketLoss").value=4;
+    if (parseInt(hasil) < 150){
+        document.getElementById("indeksDelay").value=4;
     }
-    else if (hasil < 300){
-        document.getElementById("indeksPacketLoss").value=3;
+    else if (parseInt(hasil) < 300){
+        document.getElementById("indeksDelay").value=3;
     }
-    else if (hasil < 450){
-        document.getElementById("indeksPacketLoss").value=2;
+    else if (parseInt(hasil) < 450){
+        document.getElementById("indeksDelay").value=2;
     }
-    else if (hasil >= 450){
-        document.getElementById("indeksPacketLoss").value=1;
+    else if (parseInt(hasil) >= 450){
+        document.getElementById("indeksDelay").value=1;
     }
 }
 function packetLoss(){
     var paketDikirim=parseFloat(document.getElementById("paketDikirim").value);
     var paketDiterima=parseFloat(document.getElementById("paketTerima").value);
-    var paketHilang=paketDikirim-paketDiterima;
+    var paketHilang=paketDikirim/paketDiterima;
     var hasil=(paketHilang/paketDikirim)*(100);
-    document.getElementById("hasilPacketLoss").value=hasil;
-    if (hasil =0 ){
-        document.getElementById("indeksDelay").value=4;
+    document.getElementById("hasilPacketLoss").value=parseInt(hasil);
+    if (parseInt(hasil) = 0 ){
+        document.getElementById("indeksPacketLoss").value=4;
     }
-    else if (hasil <3){
-        document.getElementById("indeksDelay").value=3;
+    else if (parseInt(hasil) < 3){
+        document.getElementById("indeksPacketLoss").value=3;
     }
-    else if (hasil <15){
-        document.getElementById("indeksDelay").value=2;
+    else if (parseInt(hasil) < 15){
+        document.getElementById("indeksPacketLoss").value=2;
     }
-    else if (hasil >= 25){
-        document.getElementById("indeksDelay").value=1;
+    else if (parseInt(hasil) >= 25){
+        document.getElementById("indeksPacketLoss").value=1;
     }
 }
 function jitter(){
@@ -59,16 +58,16 @@ function jitter(){
     var paketDiterima=parseFloat(document.getElementById("terimaPaket").value);
     hasil=totalVariasi/(paketDiterima-1);
     document.getElementById("hasilJitter").value=hasil;
-    if (hasil = 0){
+    if (parseInt(hasil) = 0){
         document.getElementById("indeksJitter").value=4;
     }
-    else if (hasil < 75){
+    else if (parseInt(hasil) < 75){
         document.getElementById("indeksJitter").value=3;
     }
-    else if (hasil < 125){
+    else if (parseInt(hasil) < 125){
         document.getElementById("indeksJitter").value=2;
     }
-    else if (hasil >= 125){
+    else if (parseInt(hasil)  >= 125){
         document.getElementById("indeksJitter").value=1;
     }
     
